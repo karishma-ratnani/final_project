6ea383ad30e6084ffba6ed7da20b89ad8d2a5f3f
mgordon14 <marissagordon61@yahoo.com>
2021-03-15 11:55:39 -0500

    second commit

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,16 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  
+    apiKey: "AIzaSyDJhYt5QZKz_Fu9OEmJhKyaXcu4zKIi6AY",
+    authDomain: "final-project-345a3.firebaseapp.com",
+    projectId: "final-project-345a3",
+    storageBucket: "final-project-345a3.appspot.com",
+    messagingSenderId: "18806877634",
+    appId: "1:18806877634:web:987e13d0010a6eac17ba5b"
+    } 
+
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
